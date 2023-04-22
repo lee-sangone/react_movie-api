6174d361085511e514c739885f2a1d485aa68d04
lee-sangone <sangwon2613@naver.com>
2023-04-22 23:40:11 +0900

    API fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState({ heroImage: "", image: "" });
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "554bd53ecdmsh7f80505d4814a44p114b4bjsn09bbcc73d52f",
+      "X-RapidAPI-Host": "transfermarket.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://transfermarket.p.rapidapi.com/clubs/get-squad?id=631&saison_id=2022&domain=de",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      if (response?.squad?.length !== 0 && response?.squad !== undefined) {
+        setImage({
+          heroImage: response.squad[0].heroImage,
+          image: response.squad[0].image,
+        });
+      }
+    })
+    .catch((err) => console.error(err));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={image.heroImage} />
+      <img src={image.image} />
     </div>
   );
 }
