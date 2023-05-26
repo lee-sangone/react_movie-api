8c1344d1f455d91fe8cfd167fb842c9b54406fd9
lee-sangone <sangwon2613@naver.com>
2023-05-26 20:36:33 +0900

    home update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Suspense, lazy } from "react";
 import MoviePage from "./movielist/list";
 import { Route, Router, Routes } from "react-router-dom";
 import DetailPage from "./moviedetail/detail";
+import Home from "./home";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/movielist" element={<MoviePage />} />
       <Route
         path="/detail/:id"
---
+++ src/home/index.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 class="homepage">홈페이지</h1>
+      <a href="/movielist">영화 검색</a>
+    </div>
+  );
+};
+
+export default Home;
